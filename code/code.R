6b1b8357382fff0a8ad8ccd8a28b3d05028ddd25
MarinaPacheco22 <61683619+MarinaPacheco22@users.noreply.github.com>
2021-02-07 23:24:40 +0100

    Minor changes

--- code/code.R
+++ code/code.R
@@ -15,7 +15,7 @@ img1 = readImage("string_sars_cov2_image.png")
 display(img1, method = "raster")
 
 #Proteinas del Sars-Cov2
-proteins.sars_cov2.table <- read.delim(file = "C:/Users/Usuario/OneDrive/Universidad/Curso 4/1º Cuatrimestre/Biología de Sistemas/Proyecto Sars-CoV2/string_protein_annotations_sars_cov2.tsv", sep = '\t')
+proteins.sars_cov2.table <- read.delim(file = "string_protein_annotations_sars_cov2.tsv", sep = '\t')
 proteins.sars_cov2.name <- data.frame(proteins.sars_cov2.table[1])
 
 #Sars-Human Network
@@ -23,5 +23,5 @@ img2 = readImage("string_sars_human_image.png")
 display(img2, method = "raster")
 
 #Proteinas del Sars-Human
-proteins.sars_human.table <- read.delim(file = "C:/Users/Usuario/OneDrive/Universidad/Curso 4/1º Cuatrimestre/Biología de Sistemas/Proyecto Sars-CoV2/string_protein_annotations_sars_human.tsv", sep = '\t')
+proteins.sars_human.table <- read.delim(file = "string_protein_annotations_sars_human.tsv", sep = '\t')
 proteins.sars_human.name <- data.frame(proteins.sars_human.table[1])
