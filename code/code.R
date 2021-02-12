3870b7e1d6cc6d144cb0bf25113d2d0417cdb11a
talal314 <49337666+talal314@users.noreply.github.com>
2021-02-12 20:34:34 +0100

    Update code.R

--- code/code.R
+++ code/code.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(linkcomm)
 
 setwd("C:/Users/Usuario/OneDrive/Universidad/Curso 4/1º Cuatrimestre/Biología de Sistemas/Proyecto Sars-CoV2")
-
+setwd("D:/UMA/4 curso/Bioinformatica/Biologia de sistemas/Trabajo final/project_template-master")
 string_db <- STRINGdb$new(
   version="11",
   species=9606,
